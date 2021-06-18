519adcfbfbba3a73e2c4cecfc0d91a81757b44a4
Oscar Martín Martín <martinm.oscar@gmail.com>
2021-06-18 15:02:54 +0200

    fix document public methods

--- packages/location_api/lib/src/models/location.dart
+++ packages/location_api/lib/src/models/location.dart
@@ -3,26 +3,40 @@ import 'package:json_annotation/json_annotation.dart';
 part 'location.g.dart';
 
 @JsonSerializable()
+
+/// Location
 class Location {
+  /// Default constructor
   const Location({
     required this.title,
     required this.latLng,
     required this.woeid,
   });
 
+  /// Json deserialize
   factory Location.fromJson(Map<String, dynamic> json) =>
       _$LocationFromJson(json);
 
+  /// Name of the location
   final String title;
   @JsonKey(name: 'latt_long')
+
+  /// Lat and long of the location
   final LatLng latLng;
+
+  /// Id of the location
   final int woeid;
 }
 
 @JsonSerializable()
+
+/// Lat and long of the location
+
 class LatLng {
+  /// Default constructor for LatLng
   const LatLng({required this.latitude, required this.longitude});
 
+  /// Json deserialize
   factory LatLng.fromJson(String? json) {
     final parts = json!.split(',');
     return LatLng(
@@ -31,6 +45,9 @@ class LatLng {
     );
   }
 
+  /// Latitude
   final double latitude;
+
+  /// Longitude
   final double longitude;
 }
