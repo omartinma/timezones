4a9fdedf2ce3ec2a3e063c368e2537e7b4dade80
Oscar Martín Martín <martinm.oscar@gmail.com>
2021-08-02 13:01:39 +0200

    modify test app

---
+++ test/app/app_test.dart
@@ -0,0 +1,42 @@
+// Copyright (c) 2021, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:time_zone_repository/time_zone_repository.dart';
+import 'package:timezones/app/app.dart';
+import 'package:timezones/time_zones/time_zones.dart';
+import '../helpers/helpers.dart';
+
+class MockTimeZoneRepository extends Mock implements TimeZoneRepository {}
+
+void main() {
+  group('App', () {
+    late TimeZoneRepository timeZoneRepository;
+
+    setUp(() {
+      timeZoneRepository = MockTimeZoneRepository();
+    });
+
+    testWidgets('renders AppView', (tester) async {
+      await tester.pumpApp(
+        App(timeZoneRepository: timeZoneRepository),
+      );
+      await tester.pump();
+      expect(find.byType(AppView), findsOneWidget);
+    });
+  });
+
+  group('AppView', () {
+    testWidgets('renders AppView', (tester) async {
+      await tester.pumpApp(AppView());
+      await tester.pump();
+      expect(find.byType(TimeZonesPage), findsOneWidget);
+    });
+  });
+}
